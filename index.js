14619f6a1a1f8097929ca703e7ac78663ea82c8e
Tom Boutell <tom@punkave.com>
2013-03-18 16:41:29 -0400

    The snippets module lives!

---
+++ index.js
@@ -0,0 +1,480 @@
+var async = require('async');
+var _ = require('underscore');
+var extend = require('extend');
+var fs = require('fs');
+
+// GUIDE TO USE
+//
+// To make it possible to manage snippets on the site, just call the Snippets
+// constructor and invoke the aposSnippetMenu local in your menu template.
+//
+// To make it possible to add a public "snippets page" that presents all snippets
+// alphabetically by title with links to drill down to individual snippets and can
+// be locked down by tag, paginated, etc., just add the snippets object to the pages
+// module via pages.addType(). You probably won't do this with snippets, but you'll
+// do it a lot with page types derived from snippets, such as blog and events.
+//
+// To add a snippets page with an alternate name in the page types menu and
+// overrides of some or all templates, set the `name`, `label` and `dirs` options when
+// invoking the snippets constructor (note that `/views`, `/public/css`, etc. are
+// automatically added as appropriate to directories in `dirs`). Then pass the
+// resulting object to addType. You can instantiate snippets as many times as
+// you need to.
+//
+// To create separate snippet repositories that are not visible in each other's
+// "manage" dialogs, set the `instance` option. The `instance` option is used to
+// set the `type` property in the `aposPages` collection and thus distinguishes
+// this data type from all others that are stored as pages (example: "blogPost").
+// End users don't see this, so change it only if your "subclass" of
+// snippets shouldn't be visible and editable in the same "pool" with others.
+//
+// Examples: snippets, blog posts and events are all separate conceptually and
+// should have separate settings for `instance`, while "faculty blogs" are just
+// an alternate presentation of the blog with some template overrides via the
+// `dirs` option and should be able to find the usual pool of blog posts.
+//
+// You can override any methods whose behavior you wish to modify with extend().
+// Notable possibilities include:
+//
+// `beforeInsert` receives the req object and a snippet about to be inserted for the
+// first time, and a callback. Modify it to add additional fields, then invoke the
+// callback with a fatal error if any. (Always sanitize rather than failing
+// if it is in any way possible.)
+//
+// `beforeUpdate` performs the same function for updates.
+//
+// `beforeDelete` gives you a chance to clean up related content when a snippet
+// is about to be deleted.
+//
+// `getDefaultTitle` returns the title used for a new snippet if no title is given.
+
+// Intended for use as a constructor:
+//
+// var snippets = new require('apostrophe-snippets')(options, function(err) { ... });
+
+module.exports = function(options, callback) {
+  var self = this;
+  // These are "public" so the object can be passed directly to pages.addType
+  self.name = options.name || 'snippets';
+  self.label = options.label || 'Snippets';
+
+  // "Protected" properties. We want modules like the blog to be able
+  // to access these, thus no variables defined in the closure
+  self._apos = options.apos;
+  self._pages = options.pages;
+  self._app = options.app;
+  self._options = options;
+  self._dirs = (options.dirs || []).concat([ __dirname ]);
+
+  console.log(self._dirs);
+  
+  // The type property of the page object used to store the snippet, also
+  // passed to views for use in CSS classes etc. Should be camel case
+  self._instance = options.instance || 'snippet';
+  // Hyphenated, all lowercase version of same, for CSS classes, permission names, URLs
+  self._css = self._apos.cssName(self._instance);
+  self._action = '/apos-' + self._css;
+
+  self.pushAsset = function(type, name) {
+    return self._apos.pushAsset(type, name, __dirname, self._action);
+  };
+
+  // Make sure that aposScripts and aposStylesheets summon our
+  // browser-side UI assets for managing snippets
+
+  self.pushAsset('script', 'main');
+  self.pushAsset('stylesheet', 'main');
+  self.pushAsset('template', 'new');
+  self.pushAsset('template', 'edit');
+  self.pushAsset('template', 'manage');
+
+  // It's possible to show a collection of recent snippets publicly
+  // on a page, and also to access permalink pages for snippets.
+  // We might use this directly, but we'll definitely use it in most
+  // modules that inherit from this one such as blog and events, which
+  // is why it is valuable for it to be supported here.
+
+  // Custom page settings template for snippet collection pages
+  self.pushAsset('template', 'pageSettings');
+
+  // Sanitize newly submitted page settings (never trust a browser)
+  extend(self, {
+    settings: {
+      sanitize: function(data, callback) {
+        var ok = {};
+        ok.tags = self._apos.sanitizeTags(data.tags);
+        return callback(null, ok);
+      }
+    }
+  }, true);
+
+  self.getDefaultTitle = function() {
+    return 'My Snippet';
+  };
+
+  self._app.post(self._action + '/insert', function(req, res) {
+    var snippet;
+    var title;
+    var content;
+    var slug;
+    var tags;
+
+    title = req.body.title.trim();
+    // Validation is annoying, automatic cleanup is awesome
+    if (!title.length) {
+      title = self.getDefaultTitle();
+    }
+    slug = self._apos.slugify(title);
+
+    content = JSON.parse(req.body.content);
+    self._apos.sanitizeItems(content);
+
+    tags = req.body.tags;
+
+    async.series([ permissions, prepare, insert ], send);
+
+    function permissions(callback) {
+      return self._apos.permissions(req, 'edit-' + self._instance, null, function(err) {
+        // If there is no permissions error then we are cool
+        // enough to create a post
+        return callback(err);
+      });
+    }
+
+    function prepare(callback) {
+      snippet = { title: title, type: self._instance, tags: tags, areas: { body: { items: content } }, slug: slug, createdAt: new Date(), publishedAt: new Date() };
+      if (self.beforeInsert) {
+        return self.beforeInsert(req, snippet, callback);
+      }
+      return callback(null);
+    }
+
+    function insert(callback) {
+      return self._apos.putPage(slug, snippet, callback);
+    }
+
+    function send(err) {
+      if (err) {
+        res.statusCode = 500;
+        return res.send('error');
+      }
+      return res.send(JSON.stringify(snippet));
+    }
+  });
+
+  self._app.post(self._action + '/update', function(req, res) {
+    var snippet;
+    var title;
+    var content;
+    var originalSlug;
+    var slug;
+    var tags;
+
+    title = req.body.title.trim();
+    // Validation is annoying, automatic cleanup is awesome
+    if (!title.length) {
+      title = self.getDefaultTitle();
+    }
+
+    tags = req.body.tags;
+
+    originalSlug = req.body.originalSlug;
+    slug = self._apos.slugify(req.body.slug);
+    if (!slug.length) {
+      slug = originalSlug;
+    }
+
+    content = JSON.parse(req.body.content);
+    self._apos.sanitizeItems(content);
+
+    async.series([ getSnippet, permissions, massage, update, redirect ], send);
+
+    function getSnippet(callback) {
+      self._apos.getPage(originalSlug, function(err, page) {
+        if (err) {
+          return callback(err);
+        }
+        if (!page) {
+          return callback('No such ' + self._instance);
+        }
+        if (page.type !== self._instance) {
+          return callback('Not a ' + self._instance);
+        }
+        snippet = page;
+        return callback(null);
+      });
+    }
+
+    function permissions(callback) {
+      return self._apos.permissions(req, 'edit-' + self._instance, post, function(err) {
+        // If there is no permissions error then we are cool
+        // enough to create a post
+        return callback(err);
+      });
+    }
+
+    function massage(callback) {
+      post.title = title;
+      post.slug = slug;
+      post.tags = tags;
+      post.areas = { body: { items: content } };
+      if (self.beforeUpdate) {
+        return self.beforeUpdate(req, post, callback);
+      }
+      return callback(null);
+    }
+
+    function update(callback) {
+      self._apos.putPage(originalSlug, post, callback);
+    }
+
+    function redirect(callback) {
+      self._apos.updateRedirect(originalSlug, slug, callback);
+    }
+
+    function send(err) {
+      if (err) {
+        res.statusCode = 500;
+        return res.send('error');
+      }
+      return res.send(JSON.stringify(post));
+    }
+  });
+
+  self._app.post(self._action + '/delete', function(req, res) {
+
+    async.series([ get, permissions, beforeDelete, deleteSnippet], respond);
+
+    var slug;
+    var snippet;
+
+    function get(callback) {
+      slug = req.body.slug;
+      return self._apos.getPage(slug, function(err, postArg) {
+        snippet = postArg;
+        if(!snippet) {
+          return callback('Not Found');
+        }
+        if (snippet.type !== self._instance) {
+          return callback('Not a ' + self._instance);
+        }
+        return callback(err);
+      });
+    }
+
+    function permissions(callback) {
+      return self._apos.permissions(req, 'delete-' + self._instance, snippet, function(err) {
+        // If there is no permissions error then we are cool
+        // enough to delete the post
+        return callback(err);
+      });
+    }
+
+    function beforeDelete(callback) {
+      if (self.beforeDelete) {
+        return self.beforeDelete(req, snippet, callback);
+      }
+      return callback(null);
+    }
+
+    function deleteSnippet(callback) {
+      self._apos.pages.remove({slug: snippet.slug}, callback);
+    }
+
+    function respond(err) {
+      if (err) {
+        return res.send(JSON.stringify({
+          status: err
+        }));
+      }
+      return res.send(JSON.stringify({
+        status: 'ok'
+      }));
+    }
+  });
+
+
+  self._app.get(self._action + '/get', function(req, res) {
+    self.get(req, req.query, function(err, snippets) {
+      return res.send(JSON.stringify(snippets));
+    });
+  });
+
+  self._app.get(self._action + '/get-one', function(req, res) {
+    self.get(req, req.query, function(err, snippets) {
+      if (snippets && snippets.length) {
+        res.send(JSON.stringify(snippets[0]));
+      } else {
+        res.send(JSON.stringify(null));
+      }
+    });
+  });
+
+  // Serve our assets. self is the final route so it doesn't
+  // beat out the rest. Note we allow overrides for assets too
+  self._app.get(self._action + '/*', self._apos.static(_.map(self._dirs, function(dir) {
+    return dir + '/public';
+  })));
+
+  var localName = self._apos.capitalizeFirst(self._instance);
+  var name = 'apos' + localName + 'Menu';
+  self._apos.addLocal(name, function(args) {
+    var result = self.render('menu', args);
+    return result;
+  });
+
+  // Render a partial, looking for overrides in our preferred places
+  self.render = function(name, data) {
+    return self.renderer(name)(data);
+  };
+
+  // Return a function that will render a particular partial looking for overrides in our
+  // preferred places
+  self.renderer = function(name) {
+    return function(data) {
+      return self._apos.partial(name, data, _.map(self._dirs, function(dir) { return dir + '/views'; }));
+    };
+  };
+
+  // Returns recent snippets the current user is permitted to read, in
+  // alphabetical order by title. If options.editable is true, only
+  // snippets the current user can edit are returned. If options.sort is
+  // present, it is passed to mongo's sort() method. All other properties of
+  // options are merged with the MongoDB criteria object used to
+  // select the relevant snippets. If options.sort is present, it is passed
+  // as the argument to the MongoDB sort() function, replacing the
+  // default alpha sort. optionsArg may be skipped.
+
+  self.get = function(req, optionsArg, callback) {
+    if (!callback) {
+      callback = optionsArg;
+      optionsArg = {};
+    }
+    var options = {};
+    extend(options, optionsArg, true);
+    // Consume options then remove them, turning the rest into mongo criteria
+    var editable = options.editable;
+    if (options.editable !== undefined) {
+      delete options['editable'];
+    }
+    var sort = options.sort || { sortTitle: 1};
+    if (options.sort !== undefined) {
+      delete options['sort'];
+    }
+    options.type = self._instance;
+    console.log("Criteria are:");
+    console.log(options);
+    // TODO: with many snippets there is a performance problem with calling
+    // permissions separately on them. Pagination will have to be performed
+    // manually after all permissions have been checked. The A1.5 permissions
+    // model wasn't perfect but it was fully integrated with the database.
+    self._apos.pages.find(options).sort(sort).toArray(function(err, snippets) {
+      if (err) {
+        return callback(err);
+      }
+      async.filter(snippets, function(snippet, callback) {
+        self._apos.permissions(req, editable ? 'edit-' + self._css : 'view-' + self._css, snippet, function(err) {
+          return callback(!err);
+        });
+      }, function(snippets) {
+        return callback(null, snippets);
+      });
+    });
+  };
+
+  // This is a loader function, for use with the `load` option of
+  // the pages module's `serve` method.
+  //
+  // If the page type group is not "snippet" (or as overridden via self._instance),
+  // self loader does nothing.
+  //
+  // Otherwise, if the page matches the URL
+  // exactly, self function serves up the "main index" page of the snippet
+  // repository (a list of snippets in alphabetical in blog order).
+  //
+  // If the page is an inexact match, self function looks at the remainder of the
+  // URL to decide what to do. If the remainder is a slug, the snippet with that
+  // slug is served (a "permalink page").
+  //
+  // "Why would you want to make snippets public like self?" You wouldn't. But in
+  // a module that inherits from self one, like the blog module, self is the starting
+  // point for serving up blogs and permalink pages.
+
+  self.loader = function(req, callback) {
+    async.series([permissions, go], callback);
+
+    function permissions(callback) {
+      // Does self person have any business editing snippets? If so make that
+      // fact available to templates so they can offer buttons to access
+      // the admin interface conveniently
+      self._apos.permissions(req, 'edit-' + self._css, null, function(err) {
+        req.extras['edit' + self._apos.capitalizeFirst(self._instance)] = !err;
+        return callback(null);
+      });
+    }
+
+    function go(callback) {
+      if (!req.bestPage) {
+        return callback(null);
+      }
+
+      // If the page type doesn't share our instance name
+      // this page isn't relevant for us
+      var type = self._pages.getType(req.bestPage.type);
+      if ((!type) || (type._instance !== self._instance)) {
+        return callback(null);
+      }
+
+      // We consider a partial match to be good enough, depending on the
+      // remainder of the URL
+      req.page = req.bestPage;
+
+      self.dispatch(req, callback);
+    }
+  };
+
+  // Often overridden when subclassing
+  self.dispatch = function(req, callback) {
+    var permalink = false;
+    if (req.remainder.length) {
+      // Perhaps it's a snippet permalink
+      req.query.slug = req.remainder;
+      permalink = true;
+    }
+
+    var criteria = {};
+    if (req.page.typeSettings && req.page.typeSettings.tags) {
+      criteria.tags = { $in: req.page.typeSettings.tags };
+    }
+    self.get(req, criteria, function(err, snippets) {
+      if (err) {
+        return callback(err);
+      }
+      console.log('here is what I got:');
+      console.log(snippets);
+      if (permalink) {
+        if (!snippets.length) {
+          req.template = 'notfound';
+        } else {
+          req.template = self.renderer('show');
+          // Generic noun so we can more easily inherit templates
+          req.extras.item = snippets[0];
+        }
+      } else {
+        req.template = self.renderer('index');
+        // Generic noun so we can more easily inherit templates
+        req.extras.items = snippets;
+      }
+      return callback(null);
+    });
+  };
+
+  if (callback) {
+    // Invoke callback on next tick so that the constructor's return
+    // value can be assigned to a variable in the same closure where
+    // the callback resides
+    process.nextTick(function() { return callback(null); });
+  }
+};
+
+
---
+++ public/js/main.js
@@ -0,0 +1,228 @@
+// NOTES FOR REUSE:
+//
+// If your custom "subclass" of snippets has a distinct setting for the instance option
+// on the server side, you must pass that option on the browser side as well.
+//
+// After calling the constructor you can $.extend it with methods as follows.
+// Note that "manager" refers to the main dialog presenting a list of many items
+// and "editor" refers to the editor dialog for just one item. "beforeUpdate" and
+// "beforeInsert" relate to the save operation of the editor dialog.
+//
+// For simplicity all CSS names and data attribute names that need to be distinct to
+// this data type end with a hyphenated version of your instance option. Note that
+// this will be singular in all cases.
+//
+// addingToManager($el, $snippet, snippet)   <-- note no callback, keep it fast & simple
+// afterPopulatingManager($el, $snippets, snippets, callback)
+// afterPopulatingEditor($el, snippet, callback) <-- Sync custom fields from snippet to $el
+// beforeUpdate($el, data) <-- Sync custom fields from $el to data
+// beforeInsert($el, data) <-- Same for new items rather than existing items
+//
+// By default, if snippets are available as a page type, a custom page settings field is
+// presented allowing the user to pick the tags used to select snippets for display.
+// You can override this behavior and add your own fields with these methods, which are
+// grouped in the settings property:
+//
+// settings.serialize($el) <-- Sync custom page settings from typeSettings[] fields of $el, return an object
+// settings.unserialize(data, $el) <-- Copy properties from data to custom page settings in typeSettings[] fields of $el
+
+function AposSnippets(optionsArg) {
+  var self = this;
+  var options = {
+    instance: 'snippet'
+  };
+  $.extend(options, optionsArg);
+  self._instance = options.instance;
+  self._css = apos.cssName(self._instance);
+  console.log("self._css is " + self._css);
+  self._action = '/apos-' + self._css;
+  self._pages = options.pages;
+
+  self.settings = {
+    serialize: function($el, $details) {
+      apos.log('serialize called');
+      var data = { tags: apos.tagsToArray($details.find('[name="typeSettings[tags]"]').val()) };
+      return data;
+    },
+    unserialize: function(data, $el, $details) {
+      $details.find('[name="typeSettings[tags]"]').val(apos.tagsToString(data.tags));
+    }
+  };
+
+  // Make a new snippet
+  $('body').on('click', '[data-new-' + self._css + ']', function() {
+    var $el = apos.modalFromTemplate('.apos-new-' + self._css, {
+      save: function(callback) {
+        return self.insertOrUpdate($el, 'insert', {}, callback);
+      },
+      init: function(callback) {
+        return self.enableArea($el, [], function() {
+          self.afterPopulatingEditor($el, {}, callback);
+        });
+      }
+    });
+    return false;
+  });
+
+  self.addingToManager = function($el, $snippet, snippet) {
+  };
+
+  self.afterPopulatingManager = function($el, $snippets, snippets, callback) {
+    return callback();
+  };
+
+  self.afterPopulatingEditor = function($el, snippet, callback) {
+    return callback();
+  };
+
+  self.beforeUpdate = function($el, data, callback) {
+    return callback();
+  };
+
+  self.beforeInsert = function($el, data, callback) {
+    return callback();
+  };
+
+  self.enableArea = function($el, content, callback) {
+    $.post('/apos/edit-virtual-area', { content: JSON.stringify(content) }, function(data) {
+        var editView = $el.find('[data-body-edit-view]');
+        editView.append(data);
+        return callback(null);
+      }
+    );
+  };
+
+  self.insertOrUpdate = function($el, action, options, callback) {
+    var data = {
+      title: $el.find('[name="title"]').val(),
+      tags: apos.tagsToArray($el.find('[name="tags"]').val()),
+      slug: $el.find('[name="slug"]').val(),
+      type: $el.find('[name="type"]').val(),
+      content: apos.stringifyArea($el.find('[data-editable]')),
+      originalSlug: options.slug
+    };
+
+    if (action === 'update') {
+      self.beforeUpdate($el, data, go);
+    } else {
+      self.beforeInsert($el, data, go);
+    }
+
+    function go() {
+      $.ajax(
+        {
+          url: self._action + '/insert',
+          data: data,
+          type: 'POST',
+          dataType: 'json',
+          success: function(data) {
+            // Let anything that cares about changes to items of this kind know
+            apos.change(self._instance);
+            return callback(null);
+          },
+          error: function() {
+            alert('Server error');
+            return callback('Server error');
+          }
+        }
+      );
+    }
+  };
+
+  // Manage all snippets
+  apos.log('configuring click for ' + '[data-manage-' + self._css + ']');
+  apos.log('length is: ' + $('[data-manage-' + self._css + ']').length);
+  $('body').on('click', '[data-manage-' + self._css + ']', function() {
+    var snippets;
+    apos.log('manage clicked');
+    $el = apos.modalFromTemplate('.apos-manage-' + self._css, {
+      init: function(callback) {
+        // We want to know if a snippet is modified
+        $el.attr('data-apos-trigger-' + self._css, '');
+        console.log('after adding attr:');
+        console.log($el[0]);
+        // Trigger an initial refresh
+        $el.trigger('apos-change-' + self._instance, callback);
+      }
+    });
+
+    // Allows things like the new snippet and edit snippet dialogs to
+    // tell us we should refresh our list
+
+    $el.on('apos-change-' + self._instance, function(e, callback) {
+      $.getJSON(self._action + '/get', { editable: true }, function(data) {
+        snippets = data;
+        $snippets = $el.find('[data-items]');
+        $snippets.find('[data-item]:not(.apos-template)').remove();
+        _.each(snippets, function(snippet) {
+          var $snippet = apos.fromTemplate($snippets.find('[data-item].apos-template'));
+          var $title = $snippet.find('[data-title]');
+          $title.text(snippet.title);
+          $title.attr('data-slug', snippet.slug);
+          self.addingToManager($snippet, snippet);
+          $snippets.append($snippet);
+        });
+        self.afterPopulatingManager($el, $snippets, snippets, function() {
+          if (callback) {
+            apos.log('calling back after populating manager');
+            apos.log('$snippets.length is ' + $snippets.length);
+            apos.log('Inserted snippets: ' + $el.find('[data-item]:not(.apos-template)').length);
+            return callback(null);
+          } else {
+            apos.log('enh no callback');
+          }
+        });
+      });
+    });
+  });
+
+  // Edit one snippet
+  $('body').on('click', '[data-edit-' + self._css + ']', function() {
+    var slug = $(this).data('slug');
+    var snippet;
+    var $el = apos.modalFromTemplate('.apos-edit-' + self._css, {
+      save: save,
+      init: function(callback) {
+        $.getJSON(self._action + '/get', { slug: slug, editable: true }, function(data) {
+          if ((!data) || (!data.length)) {
+            // TODO all alerts should get prettified into something nicer
+            alert('That item does not exist or you do not have permission to edit it.');
+            return callback('no such item');
+          }
+          snippet = data.pop();
+          apos.log(snippet);
+
+          $el.find('[name=title]').val(snippet.title);
+          $el.find('[name=tags]').val(apos.tagsToString(snippet.tags));
+          $el.find('[name=slug]').val(snippet.slug);
+
+          apos.log('BEFORE suggest slug');
+          apos.log($el[0]);
+          // name=slug must always exist, at least as a hidden field, to support this
+          apos.suggestSlugOnTitleEdits($el.find('[name=title]'), $el.find('[name=slug]'));
+
+          $el.on('click', '[data-action="delete"]', function() {
+            // TODO this should obviously be a pretty confirmation dialog
+            // with a type specific, internationalizable message
+            if (confirm('Are you sure you want to delete this permanently and forever?')) {
+              $.post(self._action + '/delete', { slug: slug }, function(data) {
+                apos.change(self._instance);
+                $el.trigger('aposModalHide');
+              }, 'json');
+            }
+            return false;
+          });
+
+          self.enableArea($el, snippet.areas.body ? snippet.areas.body.items : [], function() {
+            self.afterPopulatingEditor($el, snippet, callback);
+          });
+        });
+      }
+    });
+
+    function save(callback) {
+      return self.insertOrUpdate($el, 'update', { slug: slug }, callback);
+    }
+    return false;
+  });
+}
