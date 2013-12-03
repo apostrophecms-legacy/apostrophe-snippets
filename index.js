9de4c19d50032d28492111193f56e2eff5c1e6f9
Tom Boutell <tom@punkave.com>
2013-12-03 11:54:25 -0500

    Factored out schemas to the apostrophe-schemas module

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var _ = require('underscore');
 var extend = require('extend');
 var fs = require('fs');
 var widget = require(__dirname + '/widget.js');
-var async = require('async');
 var csv = require('csv');
 var moment = require('moment');
 var RSS = require('rss');
@@ -26,6 +25,7 @@ snippets.Snippets = function(options, callback) {
   self._searchable = (options.searchable !== undefined) ? options.searchable : true;
   self._options = options;
   self._perPage = options.perPage || 10;
+  self._schemas = options.schemas;
 
   // Set defaults for feeds, but respect it if self._options.feed has been
   // explicitly set false
@@ -227,7 +227,7 @@ snippets.Snippets = function(options, callback) {
     // etc. and also additional type-dependent properties like min and max
     // (for integers) and choices (for selects).
 
-    self.schema = [
+    options.addFields = [
       {
         name: 'title',
         label: 'Title',
@@ -278,273 +278,24 @@ snippets.Snippets = function(options, callback) {
         type: 'boolean',
         def: false
       }
-    ];
+    ].concat(options.addFields || []);
 
-    // addFields adds or overrides fields in the schema, preserving its order.
-    // If the "before" or "after" option is present, then that field is spliced in
-    // "before" or "after" the specified field, if found, and any subsequently
-    // added fields without a "before" or "after" follow that one. "end: true"
-    // and "start: true" are additional ways to change the location where the
-    // next field will appear.
-    if (options.addFields) {
-      var nextSplice = self.schema.length;
-      _.each(options.addFields, function(field) {
-        var i;
-        for (i = 0; (i < self.schema.length); i++) {
-          if (self.schema[i].name === field.name) {
-            self.schema.splice(i, 1);
-            if (!(field.before || field.after)) {
-              // Replace it in its old position if none was explicitly requested
-              self.schema.splice(i, 0, field);
-              return;
-            }
-            // before or after requested, so fall through and let them work
-            break;
-          }
-        }
-        if (field.start) {
-          nextSplice = 0;
-        }
-        if (field.end) {
-          nextSplice = self.schema.length;
-        }
-        if (field.after) {
-          for (i = 0; (i < self.schema.length); i++) {
-            if (self.schema[i].name === field.after) {
-              nextSplice = i + 1;
-              break;
-            }
-          }
-        }
-        if (field.before) {
-          for (i = 0; (i < self.schema.length); i++) {
-            if (self.schema[i].name === field.before) {
-              nextSplice = i;
-              break;
-            }
-          }
-        }
-        self.schema.splice(nextSplice, 0, field);
-        nextSplice++;
-      });
-    }
-
-    // removeFields removes fields from the schema, preserving its order
-    if (options.removeFields) {
-      self.schema = _.filter(self.schema, function(field) {
-        return !_.contains(options.removeFields, field.name);
-      });
-    }
-
-    // orderFields changes the order of fields. Any fields not specified
-    // go to the end, in their old order (see removeFields). Subclasses
-    // should honor any setting passed by a sub-subclass before their own default
-
-    if (options.orderFields) {
-      var fieldsObject = {};
-      var copied = {};
-      _.each(self.schema, function(field) {
-        fieldsObject[field.name] = field;
-      });
-      self.schema = [];
-      _.each(options.orderFields, function(name) {
-        if (fieldsObject[name]) {
-          self.schema.push(fieldsObject[name]);
-        }
-        copied[name] = true;
-      });
-      _.each(fieldsObject, function(field, name) {
-        if (!copied[name]) {
-          self.schema.push(field);
-        }
-      });
-    }
-
-    // alterFields is a custom function that alters the schema. Hopefully
-    // hardly ever used thanks to addFields, removeFields and orderFields
-    if (options.alterFields) {
-      options.alterFields(self.schema);
-    }
+    // Let apostrophe-schemas consume the addFields, removeFields,
+    // orderFields and alterFields options and produce a schema
+    // that takes subclassing into account
+    self.schema = self._schemas.compose(options);
 
     // For bc
     self.textToArea = self._apos.textToArea;
 
-    // Converters from various formats for various types. Define them all
-    // for the csv importer, then copy that as a starting point for
-    // regular forms and override those that are different (areas)
-    self.converters = {};
-    self.converters.csv = {
-      area: function(data, name, snippet, field) {
-        if (!snippet.areas) {
-          snippet.areas = {};
-        }
-        snippet.areas[name] = self.textToArea(data[name]);
-      },
-      string: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeString(data[name], field.def);
-      },
-      slug: function(data, name, snippet, field) {
-        snippet[name] = self._apos.slugify(self._apos.sanitizeString(data[name], field.def));
-      },
-      tags: function(data, name, snippet, field) {
-        var tags;
-        tags = self._apos.sanitizeString(data.tags);
-        tags = self._apos.tagsToArray(tags);
-        snippet[name] = tags;
-      },
-      boolean: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeBoolean(data[name], field.def);
-      },
-      select: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeSelect(data[name], field.choices, field.def);
-      },
-      integer: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeInteger(data[name], field.def, field.min, field.max);
-      },
-      float: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeFloat(data[name], field.def, field.min, field.max);
-      },
-      url: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeUrl(data[name], field.def);
-      },
-      date: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeDate(data[name], field.def);
-      },
-      time: function(data, name, snippet, field) {
-        snippet[name] = self._apos.sanitizeTime(data[name], field.def);
-      },
-      password: function(data, name, snippet, field) {
-        // Change the stored password hash only if a new value
-        // has been offered
-        var _password = self._apos.sanitizeString(data.password);
-        if (_password.length) {
-          snippet[name] = self._apos.hashPassword(data.password);
-        }
-      }
-    };
-    // As far as the server is concerned a singleton is just an area
-    self.converters.csv.singleton = self.converters.csv.area;
-
-    self.converters.form = {};
-    extend(self.converters.form, self.converters.csv, true);
-
-    self.converters.form.singleton = self.converters.form.area = function(data, name, snippet) {
-      var content = [];
-      try {
-        content = JSON.parse(data[name]);
-      } catch (e) {
-        // Always recover graciously and import something reasonable, like an empty area
-      }
-      self._apos.sanitizeItems(content);
-      if (!snippet.areas) {
-        snippet.areas = {};
-      }
-      snippet.areas[name] = { items: content };
-    };
-
-    self.converters.form.joinByOne = function(data, name, snippet, field) {
-      snippet[field.idField] = self._apos.sanitizeId(data[name]);
-    };
-
-    self.converters.form.joinByOneReverse = function(data, name, snippet, field) {
-      // Not edited on this side of the relation
-    };
-
-    self.converters.form.joinByArray = function(data, name, snippet, field) {
-      var input = data[name] || [];
-      if (!Array.isArray(input)) {
-        input = [];
-      }
-      snippet[field.idsField] = [];
-      if (field.extras) {
-        snippet[field.extrasField] = {};
-      }
-
-      // Clear old values before we sanitize new, so we don't get orphans
-      if (field.relationshipsField) {
-        snippet[field.relationshipsField] = {};
-      }
-      // Each element may be an id or an object with a 'value' property
-      // containing the id as well as optional extra properties
-      _.each(input, function(e) {
-        var id;
-        if (typeof(e) === 'object') {
-          id = e.value;
-        } else {
-          id = e;
-        }
-        id = self._apos.sanitizeId(id);
-        if (id !== undefined) {
-          snippet[field.idsField].push(id);
-          if (field.relationship) {
-            if (typeof(e) !== 'object') {
-              // Behave reasonably if we got just ids instead of objects
-              e = {};
-            }
-            // Validate the relationship (aw)
-            var validatedRelationship = {};
-            _.each(field.relationship, function(attr) {
-              if (attr.type === 'string') {
-                validatedRelationship[attr.name] = self._apos.sanitizeString(e[attr.name]);
-              } else if (attr.type === 'boolean') {
-                validatedRelationship[attr.name] = self._apos.sanitizeBoolean(e[attr.name]);
-              } else if (attr.type === 'select') {
-
-                validatedRelationship[attr.name] = self._apos.sanitizeSelect(e[attr.name], attr.choices);
-              } else {
-                console.log(snippet.name + ': unknown type for attr attribute of relationship ' + name + ', ignoring');
-              }
-            });
-            snippet[field.relationshipsField][id] = validatedRelationship;
-          }
-        }
-      });
-    };
-
-    self.converters.form.joinByArrayReverse = function(data, name, snippet, field) {
-      // Not edited on this side of the relation
-    };
-
-    self.converters.form.tags = function(data, name, snippet, field) {
-      snippet[name] = self._apos.sanitizeTags(data.tags);
-    };
-
+    // Convenience wrapper for schemas.convertFields
     self.convertAllFields = function(from, data, snippet) {
-      return self.convertSomeFields(self.schema, from, data, snippet);
+      return self._schemas.convertFields(self.schema, from, data, snippet);
     };
 
+    // For bc, now wraps schemas.convertFields
     self.convertSomeFields = function(schema, from, data, snippet) {
-      _.each(schema, function(field) {
-        // Support for legacy field names, which makes it easier to support
-        // legacy new.html and edit.html templates for things like the blog
-        if (field.legacy && (data[field.legacy] !== undefined)) {
-          data[field.name] = data[field.legacy];
-        }
-        self.converters[from][field.type](data, field.name, snippet, field);
-      });
-    };
-
-    // Make each type of schema field searchable. You can shut this off
-    // for any field by setting its `search` option to false. Not all
-    // field types make sense for search. Areas and singletons are always
-    // searchable. The `weight` option makes a property more significant
-    // in search; in the current implementation weights greater than 10
-    // are treated more prominently. By default all schema fields are
-    // treated as more important than ordinary body text. You can change
-    // that by setting a lower weight. The "silent" option, which is true
-    // by default, prevents the field from showing up in the summary of
-    // the item presented with search results.
-
-    self.indexers = {
-      string: function(value, field, texts) {
-        var silent = (field.silent === undefined) ? true : field.silent;
-        texts.push({ weight: field.weight || 15, text: value, silent: silent });
-      },
-      select: function(value, field, texts) {
-        var silent = (field.silent === undefined) ? true : field.silent;
-        texts.push({ weight: field.weight || 15, text: value, silent: silent });
-      }
-      // areas and singletons are always indexed
+      return self._schemas.convertFields(schema, from, data, snippet);
     };
 
     self.importCreateItem = function(req, data, callback) {
@@ -552,7 +303,6 @@ snippets.Snippets = function(options, callback) {
       // try/catch of its own that is making it impossible to log any
       // errors if we don't catch them. TODO: go looking for that and fix it.
       try {
-        var published = self._apos.sanitizeBoolean(data.published, true);
 
         var snippet = {
           type: self._instance,
@@ -612,7 +362,7 @@ snippets.Snippets = function(options, callback) {
           return (field.name !== 'slug');
         });
 
-        snippet = { type: self._instance, areas: {}, createdAt: new Date() };
+        snippet = self.newInstance();
 
         self.convertSomeFields(fields, 'form', req.body, snippet);
 
@@ -1032,15 +782,7 @@ snippets.Snippets = function(options, callback) {
 
     self._apos.addListener('index', function(snippet, lines) {
       if (snippet.type === self._instance) {
-        _.each(self.schema, function(field) {
-          if (field.search === false) {
-            return;
-          }
-          if (!self.indexers[field.type]) {
-            return;
-          }
-          self.indexers[field.type](snippet[field.name], field, lines);
-        });
+        self._schemas.indexFields(self.schema, snippet, lines);
         // Custom search indexing
         self.addSearchTexts(snippet, lines);
       }
@@ -1239,23 +981,6 @@ snippets.Snippets = function(options, callback) {
       ]
     };
 
-    // Used to implement 'join', below
-
-    var joinrs = {
-      joinByOne: function(field, options, callback) {
-        return self._apos.joinByOne(req, results.snippets, field.idField, field.name, options, callback);
-      },
-      joinByOneReverse: function(field, options, callback) {
-        return self._apos.joinByOneReverse(req, results.snippets, field.idField, field.name, options, callback);
-      },
-      joinByArray: function(field, options, callback) {
-        return self._apos.joinByArray(req, results.snippets, field.idsField, field.relationshipsField, field.name, options, callback);
-      },
-      joinByArrayReverse: function(field, options, callback) {
-        return self._apos.joinByArrayReverse(req, results.snippets, field.idsField, field.relationshipsField, field.name, options, callback);
-      }
-    };
-
     return async.series([ query, join, metadata, permalinker ], function(err) {
       return mainCallback(err, results);
     });
@@ -1279,77 +1004,7 @@ snippets.Snippets = function(options, callback) {
 
     function join(callback) {
       var withJoins = options.withJoins;
-      if (withJoins === false) {
-        // Joins explicitly deactivated for this call
-        return callback(null);
-      }
-      if (!results.snippets.length) {
-        // Don't waste effort
-        return callback(null);
-      }
-      // Only interested in joins
-      var joins = _.filter(self.schema, function(field) {
-        return !!joinrs[field.type];
-      });
-      if (results.snippets.length > 1) {
-        // Only interested in joins that are not restricted by ifOnlyOne.
-        // This mechanism saves time and memory in cases where you don't need
-        // the results of the join in index views
-        joins = _.filter(joins, function(join) {
-          return !join.ifOnlyOne;
-        });
-      }
-      // The withJoins option allows restriction of joins. Set to false
-      // it blocks all joins. Set to an array, it allows the joins named within.
-      // If some of those names use dot notation, a chain of nested joins to be
-      // permitted can be specified.
-      //
-      // By default, all configured joins will take place, but withJoins: false
-      // will be passed when fetching the objects on the other end of the join,
-      // so that infinite recursion never takes place.
-
-      var withJoinsNext = {};
-      // Explicit withJoins option passed to us
-      if (Array.isArray(withJoins)) {
-        joins = _.filter(joins, function(join) {
-          var winner;
-          _.each(withJoins, function(withJoinName) {
-            if (withJoinName === join.name) {
-              winner = true;
-            }
-            if (withJoinName.substr(0, join.name.length + 1) === (join.name + '.')) {
-              if (!withJoinsNext[join.name]) {
-                withJoinsNext[join.name] = [];
-              }
-              withJoinsNext[join.name].push(withJoinName.substr(join.name.length + 1));
-              winner = true;
-            }
-          });
-          return winner;
-        });
-      } else {
-        // No explicit withJoins option for us, so we do all the joins
-        // we're configured to do, and pass on the withJoins options we
-        // have configured for those
-        _.each(joins, function(join) {
-          if (join.withJoins) {
-            withJoinsNext[join.name] = join.withJoins;
-          }
-        });
-      }
-      return async.eachSeries(joins, function(join, callback) {
-        if (!join.name.match(/^_/)) {
-          console.error('WARNING: joins should always be given names beginning with an underscore (_). Otherwise you will waste space in your database storing the results');
-        }
-        var options = {
-          get: self._pages.getManager(join.withType).get,
-          getOptions: {
-            withJoins: withJoinsNext[join.name] || false,
-            permalink: true
-          }
-        };
-        return joinrs[join.type](join, options, callback);
-      }, callback);
+      return self._schemas.join(req, self.schema, results.snippets, withJoins, callback);
     }
 
     function metadata(callback) {
@@ -1912,6 +1567,10 @@ snippets.Snippets = function(options, callback) {
         return callback(null);
       });
     }, callback);
+  };
+
+  self.newInstance = function() {
+    return { type: self._instance, areas: {}, createdAt: new Date() };
   };
 
   // Sanitize newly submitted page settings (never trust a browser)
